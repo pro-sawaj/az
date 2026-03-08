00ce87349f283181fbe9c0482076dc00f0c6f8f8
Sawaj G <prosawaj@gmail.com>
2026-03-08 02:13:53 +0000

    py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-
+print("==========================================")
+print("  System Online: Developer SAWAJ G        ")
+print("  Project AZ is ready for action!         ")
+print("==========================================")
